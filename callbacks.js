0961e1521048793c22ecd16cdbe4e8fde358710d
Adena Glasper <lefteye_50@Adenas-MacBook-Pro.local>
2022-06-24 19:46:32 -0700

    Week 4 Callback Updates

--- callbacks.js
+++ callbacks.js
@@ -1,7 +1,9 @@
-let username = sentHttpRequest("getUser");
-console.log(username);
+let username = sentHttpRequest("getUser");//normall how we write code
+console.log(username);//normally how we write code
 
 function logUsername(user) {
     console.log(user);
 }
-sendHttpRequest("getUser");
+sendHttpRequest("getUser", logUsername);//this is not us invoking the function, this is the callback that passes through the function and will call upon/invoke function for us.
+
+sendHttpRequest("getUser", user => console.log(user));
